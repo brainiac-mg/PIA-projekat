20b2887499e424a206646234fd8aed939624450d
brainiac-mg <brainiac.mg@gmail.com>
2017-08-02 18:51:59 +0200

    Initial commit

---
+++ src/java/beans/Controler.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Brana
+ */
+@Named(value = "controler")
+@SessionScoped
+public class Controler implements Serializable {
+
+    private String errormsg;
+    private String usr;
+    private String pass;
+
+    public String getErrormsg() {
+        return errormsg;
+    }
+
+    public void setErrormsg(String errormsg) {
+        this.errormsg = errormsg;
+    }
+
+    public String getUsr() {
+        return usr;
+    }
+
+    public void setUsr(String usr) {
+        this.usr = usr;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+    public String LogIn(){
+        errormsg = "Greska";
+        return "index";
+    }
+    
+    public String Skip(){
+        return "guest";
+    }
+    
+    public Controler() {
+    }
+    
+}
